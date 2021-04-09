fbd17fab5b3c7009e5757af89cbf9bd030c06d6a
pitonmarquise <pitonmarquise@gmail.com>
2021-04-09 06:45:13 -0400

    added 2 new functions

--- api.php
+++ api.php
@@ -1,21 +1,26 @@
 <?php
-
 /* 
-CRUD: 
-Create
-      Read/Edit
-          Update
-                Delete
+CRUD Functions -> 
+                  Create: 
+                            AddHero 
+                  Read/Edit: 
+                            getAllHeroes
+                  Update:  
+                            createBattle 
+                  Delete: 
+                            deleteHero
 =============================================== 
                                                +                                     
            CONNECTION PROCESS                  +                                     
                                                +                      
 =============================================== 
 */
-  // input the credientials to connect to the db server. 
+  // input the credientials to connect to the db server.
+  // Using phpMyAdmin. 
   $dbhost = 'localhost';                                                             
   $dbuser = 'root';
-  $dbpassword = 'root';
+  // No password just for educational purposes. 
+  $dbpassword ='';
   $dbname = 'sqlheroes';
 
 // Create connection to the database
@@ -27,7 +32,7 @@ $mysqli = new mysqli(
                   // Database password
                   $password, 
                   // Database name
-                  $dbname)
+                  $dbname);
   
   // check the connection 
   if ($mysqli->connect_error) {
@@ -38,56 +43,142 @@ $mysqli = new mysqli(
     exit();
   }
   //Run this if the connection is good. 
-  echo "Connected Sucessfully";     
-} 
+  echo 'Connected Sucessfully';  
+//============================================
+ 
 /*
 connecting to the db server and running the 
 command that is saved in "$sql" and saving the 
 results in "$result". 
 */
-$route = $_GET['route']
+$route = $_GET['route'];
   
-  
+/* Depending on which link the user will click.   
+=============================================== 
+                                               +                                     
+                 Route                         +                                     
+                                               +                      
+=============================================== 
+When the user clicks on a link. It will run the 
+associated functino given them the data that they
+asksed for. 
+*/
 switch ($route){
-  case "getAllHeroes":
+  case 'getAllHeroes':
     $myData = getAllHeroes($mysqli);
     break; 
-  case "createBattle":
+  case 'createBattle':
     $myData = createBattle($mysqli,2,4,4);
     break;
-  case "getHeroByID": 
-    $myData = getHeroByID($mysqli, $id);
+  case 'getHeroByID': 
+    // Should return the hero "The Seer"
+    $myData = getHeroByID($mysqli,5);
     break;
+    // Should delete the hero "The Seer" 
+  case 'deleteHero':
+    $myData = deleteHero($mysqli, 5);
+    break; 
+  case 'addHero':
+    $myData = addHero($mysqli);
+    break; 
   default:
     $myData = json_encode([]);
 }
-echo $myData
-  
-  
-// Closing the connections.  
+
+echo $myData; 
+ 
+  // Closing the connections.  
 $mysqli->close();
+//=============================================
+/*
+=============================================== 
+                                               +                                     
+                 Function                      +                                     
+                                               +                      
+=============================================== 
+   Get Every single hero is the database 
+   If the user would like to see every hero 
+   from the database. 
+ */
 function getAllHeroes($mysqli){
+  // Will put the data in an array object 
   $data=array();
   // Saving the query command in "$sql"
-  $sql = "SELECT * FROM heroes";
+  /*
+  Explanantion: 
+                I'm selecting every row from the hero table.
+                Including all columns, asterisk: means everything
+                in the column. 
+  */
+  $sql = 'SELECT * 
+          FROM heroes';
+  // Stores the query output in a variable
+  $result = $mysqli->query($sql);
   
+  // If the conditions are true, it will push the row into the array data 
   if ($result->num_rows > 0){
+     // fetch_assoc() function fetches a result row as an associative array.
      while($row = $result->fetch_assoc()){
-     echo "id: " , $row["id"], " Name:", $row ["firstname"], " ", $row["lastname"], "<br>";
+        // Pushes the row into the array
+        array_push($data,$row);
+    }
   }
+  // Transforms the data into a json format. 
+  return json_encode($data);
 }
+// Creates a battle between two heroes.  
 function createBattle ($mysqli, $h1, $h2, $w){
-  $sql = "INSERT INTO battles (hero1,hero2,winner)";
-  VALUES ($h1, $h2, $h3);
+  $sql = "INSERT INTO battles (hero1,hero2,winner) 
+  VALUES ($h1, $h2, $w)";
   
   if ($mysqli->query($sql) === True){
-    $record = "{'success':'created new battle'}"
+    $record = "{'success':'created new battle'}";
   }else{
-    $record = "{'Failed' : 'Wasn't able to create a new battle.}"
+    echo "{'error': '" . $sql . " - " . $mysqli->error . "'}";
   }
+  // Transforms the data into a json format.
+  return json_encode([$record]);
 }
-
-
-
+// Get the hero base on their ID. 
+function getHeroByID($mysqli,$id){
+  $data=array();
+  /* Explanation: 
+                  It selects everything from hero
+                  That has the called ID number. 
+  */
+  $sql="SELECT * 
+        FROM heroes
+        WHERE id= " . $id;
+  $result = $mysqli->query($sql);
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      array_push($data,$row);
+    }
+  }
+  // Transforms the data into a json format.
+  return json_encode($data);
+}
+// Deletes the hero base on their ID. 
+function deleteHero($mysqli, $id){
+  $data=array(); 
+  
+  $sql = "DELETE 
+          FROM heroes
+          WHERE id= " . $id;
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      array_push($data,$row);
+    }
+  }
+  return json_encode($data,$row);
+}
+// Creates a new hero 
+function addHero($mysqli){
+  $data=array();  
+  
+  $sql = "UPDATE heroes
+          SET column1=value, column2=value2,...
+          WHERE some_column=some_value ";
+}
 ?>
     
--- index.php
+++ index.php
@@ -4,19 +4,26 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!--  CSS Style    -->
+     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <title>Heroebook</title>
   </head>
   <body>
-  <div class="card" style="width: 18rem;">
+  <div class="card" style="width: 18rem; ">
   <div class="card-header"><h1>Main Menu</h1></div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">All Heroes</li>
-    <li class="list-group-item">Get Heroe by ID</li>
-    <li class="list-group-item">Made by Marquise Piton</li>
+     <li class="list-group-item"><a href='/api.php?route=getAllHeroes' target='_blank'>All Heroes</a></li>
+     <li class="list-group-item"><a href="/api.php?route=getHeroByID&id=5">Get Hero By ID</a></li>
+     <li class="list-group-item"><a href="/api.php?route=createBattle">Create Battle</a></li>
+     <li class="list-group-item"><a href="/api.php?route=addHero">Add Hero</a></li>
+     <li class="list-group-item"><a href="/api.php?route=deleteHero&id=5">Delete Hero</a></li>
+     <li class="list-group-item">Made by Marquise Piton</li>
   </ul>
   </div>
   </body>
 </html>
+
