823218eccdbe86de769ff3a582d09140676c2a95
pitonmarquise <pitonmarquise@gmail.com>
2021-04-08 21:48:42 -0400

    modified index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!--  CSS Style    -->
+     <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Heroebook</title>
+  </head>
+  <body>
+  <div class="card" style="width: 18rem;">
+  <div class="card-header"><h1>Main Menu</h1></div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">All Heroes</li>
+    <li class="list-group-item">Get Heroe by ID</li>
+    <li class="list-group-item">Made by Marquise Piton</li>
+  </ul>
+  </div>
+  </body>
+</html>
